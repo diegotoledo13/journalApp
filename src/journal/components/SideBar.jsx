32fb7cf7833c45b6fff4544ecbe91e062b8a1796
diegotoledo13 <diegomasteruno13@gmail.com>
2024-02-07 11:17:38 -0300

    trabajando en estilos

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,70 @@
+import { TurnedInNot } from "@mui/icons-material";
+import {
+  Drawer,
+  Box,
+  Toolbar,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Grid,
+  ListItemText,
+} from "@mui/material";
+import React from "react";
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Diego Toledo
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {[
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre",
+          ].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText
+                    secondary={"lorem loerem impsum le laas mitran lecon acont"}
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,8 +1,14 @@
 import { Box } from "@mui/material";
+import { NavBar } from "../components/NavBar";
+import { SideBar } from "../components/SideBar";
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {children}
       </Box>
