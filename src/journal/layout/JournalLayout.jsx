2b15313e07fec391aece7daef01f2d7aec684fe0
diegotoledo13 <diegomasteruno13@gmail.com>
2024-02-15 13:57:51 -0300

    retomando estudios y retocando cosas

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 import { NavBar } from "../components/NavBar";
 import { SideBar } from "../components/SideBar";
 
@@ -10,6 +10,7 @@ export const JournalLayout = ({ children }) => {
       <NavBar drawerWidth={drawerWidth} />
       <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView } from "../views";
+//import NothingSelectedView from "../views/NothingSelectedView";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>JournalPage</Typography>
+      {/*<NothingSelectedView />*/}
+      <NoteView />
     </JournalLayout>
   );
 };
