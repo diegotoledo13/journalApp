f2ba366ab7a21c4904cf8f87a1f6cca7fcaba5ab
diegotoledo13 <diegomasteruno13@gmail.com>
2024-03-21 16:12:19 -0300

    trabajando en la auht

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,22 @@
+import { useDispatch } from "react-redux";
 import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView } from "../views";
 import NothingSelectedView from "../views/NothingSelectedView";
 import { AddOutlined } from "@mui/icons-material";
+import { startNewNote } from "../../store/journal";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const onClickNewNotes = () => {
+    dispatch(startNewNote());
+  };
   return (
     <JournalLayout>
       <NothingSelectedView />
       {/*<NoteView />*/}
       <IconButton
+        onClick={onClickNewNotes}
         size="large"
         sx={{
           color: "white",
